bbea5bee02443e71e0444021d1b7da904417f7ad
unknown <2023003230@aluno.riogrande.ifrs.edu.br>
2025-04-15 16:48:58 -0300

    General code cleaning; Created animation for rolling, winning and losing; Added sound effects for winning and losing

--- src/views/Slots.vue
+++ src/views/Slots.vue
@@ -1,13 +1,18 @@
 <script lang="ts">
 
-import { MAX_WEIGHT } from '../config/constants';
-
 const INITIAL_SLOT = {
   value: 5,
   name: "-",
-  weight: 5,
+  weight: 0,
   class: ""
 }
+
+const MAX_WEIGHT = 5;
+const TOTAL_ROLL_TIME = 4040;
+const ROLL_SONG_TIME = 2840;
+const BUTTON_PRESS_SOUND_TIME = 700;
+const DELAY_BETWEEN_SLOTS = 400;
+const DELAY_BETWEEN_NEXT_IMAGE = 15;
 
 interface IOption {
   value: number,
@@ -17,41 +22,43 @@ interface IOption {
 }
 
 export default {
-
   data() {
     return {
       credits: 150 as number,
+      celebrating: false as boolean,
+      failing: false as boolean,
+      creditsAwarded: "" as string,
       options: [
         INITIAL_SLOT,
         {
-          value: 3,
-          name: "...",
-          weight: 5,
-          class: ""
-        },
-        {
-          value: 1,
-          name: ":/",
-          weight: 5,
-          class: ""
-        },
-        {
           value: 50,
-          name: ":)",
+          name: ":|",
           weight: 5,
+          class: ""
+        },
+        {
+          value: 100,
+          name: ":O",
+          weight: 5,
+          class: "win"
+        },
+        {
+          value: 500,
+          name: ":)",
+          weight: 4,
           class: "win"
         },
         {
           value: -100,
           name: "X",
-          weight: 4,
+          weight: 2,
           class: "lose"
         },
         {
           value: 10000,
           name: "7",
-          class: "lucky",
-          weight: 3
+          class: "win lucky",
+          weight: 2
         }] as IOption[],
 
       fields: [
@@ -65,11 +72,27 @@ export default {
 
   methods: {
     resetSlots() {
-      this.fields = [
-        INITIAL_SLOT,
-        INITIAL_SLOT,
-        INITIAL_SLOT
-      ]
+      this.fields = this.fields.map(() => {
+        return INITIAL_SLOT;
+      })
+    },
+
+    randomizeSlot(currentSlot: number, timer = ROLL_SONG_TIME) {
+        const allowedOptions:IOption[] = [];
+
+        this.options.forEach((option) => {
+          for (let i = 0; i < option.weight; i++) {
+            allowedOptions.push(option);
+          }
+        });
+        const RNG:number = Math.floor(Math.random() * allowedOptions.length);
+
+        setTimeout(() => {
+          this.fields[currentSlot] = allowedOptions[RNG];
+          if (timer >= (DELAY_BETWEEN_SLOTS * ((this.fields.length-1) - currentSlot))) {
+            this.randomizeSlot(currentSlot, timer - DELAY_BETWEEN_NEXT_IMAGE);
+          }
+        }, DELAY_BETWEEN_NEXT_IMAGE)
     },
 
     roll() {
@@ -79,39 +102,73 @@ export default {
       this.resetSlots();
       this.credits--;
 
-      var audio = new Audio('slots.mp3')
+      let audio = new Audio('slots.mp3')
       audio.play()
 
       setTimeout(() => {
-        const RNG:number[] = [];
-        const weightPick:number[] = [];
-
-        this.fields.map(async (field, index) => {
-          weightPick[index] = Math.ceil(Math.random() * MAX_WEIGHT);
-
-          RNG[index] = Math.floor(Math.random() * this.options.filter((option: IOption) => option.weight >= weightPick[index]).length);
-
-          setTimeout(() => {
-            return (
-              this.fields[index] = this.options[RNG[index]]
-              )
-            }, 400 * index)
+        this.fields.forEach((field, index) => {
+          this.randomizeSlot(index);
         })
-      }, 2840)
+      },  BUTTON_PRESS_SOUND_TIME)
 
-      setTimeout(() => this.checktrio(), 4040);
+      setTimeout(() => this.checktrio(), TOTAL_ROLL_TIME);
+    },
+
+    celebrate() {
+      this.celebrating = true;
+      
+      let audio = new Audio('good-roll.mp3')
+      audio.play()
+    
+      setTimeout(() => {
+        this.celebrating = false;
+      }, 1500);
+    },
+
+    fail() {
+      this.failing = true;
+      
+      let audio = new Audio('bad-roll.mp3')
+      audio.play()
+      
+      setTimeout(() => {
+        this.failing = false;
+      }, 1500);
     },
 
     checktrio() {
-      if (this.fields[0].name == this.fields[1].name && this.fields[0].name == this.fields[2].name) {
+      let previousSlot:IOption;
+      let match:boolean = true;
+      this.fields.forEach((field) => {
+        if (previousSlot && previousSlot != field) {
+          match = false;
+          return;
+        }
+
+        previousSlot = field;
+      })
+
+      if (match) {
+        if (this.fields[0].value > 0) {
+          this.creditsAwarded = `+ ${this.fields[0].value}`;
+          this.celebrate();
+        } else {
+          this.creditsAwarded = `- ${this.credits > Math.abs(this.fields[0].value) ? Math.abs(this.fields[0].value) : this.credits}`;
+          this.fail();
+        }
         this.credits = this.credits + this.fields[0].value;
       }
-      if (this.credits === 0) {
-        this.disabled = true;
-      } else {
-        this.disabled = false;
-      }
-    }
+      this.credits = Math.max(0, this.credits);
+
+      setTimeout(() => {
+        if (this.credits <= 0) {
+          this.disabled = true;
+        } else {
+          this.disabled = false;
+        }
+        this.creditsAwarded = "";
+      }, match ? 2000 : 0);
+  }
   },
 
   mounted() {
@@ -122,143 +179,18 @@ export default {
 <template>
   <div class="main">
     <div class="slots">
+      <div :class="[{creditsGained: !!creditsAwarded}, fields[0].class]">{{ creditsAwarded }}</div> 
       <template v-for="field in fields">
-        <div class="slotField" :class="field.class"><span class="slotName">{{ field.name }}</span></div>
+        <div id="slotField" class="slotField" :class="[{ celebrating, failing }, field.class]"><span class="slotName">{{ field.name }}</span></div>
       </template>
     </div>
     
     <br />
 
-    <div class="clickDiv"><button :disabled=(disabled) class="click" @click="roll()"></button></div>
+    <div class="clickDiv"><button :disabled=(disabled) class="click" :class="{clickDisabled: disabled}" @click="roll()"></button></div>
   </div>
 
   <span class="creditsSpan">Credits: {{ credits }}</span>
 </template>
 
-<style>
-.main {
-  min-width: 75vw;
-}
-
-.creditsSpan {
-  color: white;
-  position: absolute;
-  font-size: 1.5em;
-  bottom: 10px;
-}
-
-.clickDiv {
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-}
-
-.click {
-  font-size: 1.5em;
-  font-weight: bold;
-  cursor: pointer;
-  padding: 25px;
-  background-color: gold;
-  border: 5px solid rgb(194, 165, 0);
-  border-radius: 100px;
-  color: white;
-  text-align: center
-}
-
-.click:hover {
-  background-color: rgb(194, 165, 0);
-}
-
-.slots {
-  display: flex;
-  min-width: 100%;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-}
-
-.slotField {
-  width: 300px;
-  height: 300px;
-  border: 10px solid gold;
-  box-shadow: 3px 3px goldenrod;
-  background-color: white;
-  color: black;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-}
-
-.slotName {
-  font-size: 5em;
-  font-weight: bolder;
-}
-
-.lose {
-  color: red;
-}
-
-.win {
-  color: greenyellow;
-}
-
-.lucky {
-  animation: color-change 5s infinite;
-}
-
-@keyframes color-change {
-
-  100%,
-  0% {
-    color: rgb(255, 0, 0);
-  }
-
-  8% {
-    color: rgb(255, 127, 0);
-  }
-
-  16% {
-    color: rgb(255, 255, 0);
-  }
-
-  25% {
-    color: rgb(127, 255, 0);
-  }
-
-  33% {
-    color: rgb(0, 255, 0);
-  }
-
-  41% {
-    color: rgb(0, 255, 127);
-  }
-
-  50% {
-    color: rgb(0, 255, 255);
-  }
-
-  58% {
-    color: rgb(0, 127, 255);
-  }
-
-  66% {
-    color: rgb(0, 0, 255);
-  }
-
-  75% {
-    color: rgb(127, 0, 255);
-  }
-
-  83% {
-    color: rgb(255, 0, 255);
-  }
-
-  91% {
-    color: rgb(255, 0, 127);
-  }
-
-}
-</style>
+<style lang="css" src="./slots.css"/>
